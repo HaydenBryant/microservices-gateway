fd3339b770f27f812171ef3188375ab70560a144
Hayden Bryant <haydenbryant.student@careerdevs.com>
2020-12-06 15:50:32 -0500

    this works. It was the api giving me issues

--- src/main/java/com/cloudgatewayservice/demo/SpringCloudConfig.java
+++ src/main/java/com/cloudgatewayservice/demo/SpringCloudConfig.java
@@ -10,21 +10,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringCloudConfig {
 
-//    @Value("${api.key}")
-//    private String apiKey;
-    @Autowired
-    private NewsApiController controller;
+    @Value("${api.key}")
+    private String apiKey;
+//    @Autowired
+//    private NewsApiController controller;
 
     @Bean
     public RouteLocator gatewayRoutes(RouteLocatorBuilder builder){
         return builder.routes()
                 .route(r -> r.path("/topHeadlines")
-                        .uri(controller.getTopHeadlines()))
-//                        .id("employeeModule"))
+                        .uri("http://newsapi.org/v2/top-headlines?country=us&apiKey=" + apiKey))
 
-                .route(r -> r.path("/search/**")
-                        .uri("http://localhost:8082/"))
-//                        .id("consumerModule"))
+                .route(r -> r.path("/consumer/**")
+                        .uri("http://localhost:8082"))
+
                 .build();
     }
 }
